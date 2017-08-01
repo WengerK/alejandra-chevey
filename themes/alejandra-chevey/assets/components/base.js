1a90ee623a0e39f0d9723e1bf54b0111f6684f4e
Kevin Wenger <wenger.kev@gmail.com>
2017-08-01 16:21:50 +0200

    add waypoints + homepage demo

--- themes/alejandra-chevey/assets/components/base.js
+++ themes/alejandra-chevey/assets/components/base.js
@@ -3,5 +3,18 @@
 
 import '../icons/svg-icons.js';
 import BarbaJS from './barba.js';
+import Waypoints from './waypoints.js';
+
+/* global Barba */
 
 BarbaJS();
+
+function init() {
+  Waypoints();
+}
+
+Barba.Dispatcher.on('transitionCompleted', function (currentStatus, oldStatus, container) {
+  init();
+});
+
+document.addEventListener('DOMContentLoaded', init);
---
+++ themes/alejandra-chevey/assets/components/waypoints.js
@@ -0,0 +1,14 @@
+/* global Waypoint */
+
+const Waypoints = () => {
+  $('.page-section-animation')
+    .waypoint(function (direction) {
+      if (direction === 'down') {
+        $(this.element).addClass('is-view');
+      }
+    }, {
+      offset: '80%',
+    });
+};
+
+export default Waypoints;
